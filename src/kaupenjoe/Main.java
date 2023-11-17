cfc7f30ec77708730a7588a966bf7257c8db73fc
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-11-17 15:17:38 +0100

    initial commit

---
+++ src/kaupenjoe/Main.java
@@ -0,0 +1,8 @@
+package kaupenjoe;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello this is a cool YouTube Series for learning Java!");
+
+    }
+}
