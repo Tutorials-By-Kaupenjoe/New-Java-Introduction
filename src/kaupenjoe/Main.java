5e23a193ab77f7b86fdfecd067fbbf074313a777
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-11-17 16:04:41 +0100

    java syntax

--- src/kaupenjoe/Main.java
+++ src/kaupenjoe/Main.java
@@ -2,13 +2,7 @@ package kaupenjoe;
 
 public class Main {
     public static void main(String[] args) {
-        /* DATA TYPES & VARIABLES */
-
-        // Hello I am a comment
-        /*
-         * And I am a multiline Comment!
-         * I can come in quite handy for explaining stuff!
-         */
+        /* BASIC JAVA SYNTAX */
 
         /* Variables */
 
@@ -16,33 +10,11 @@ public class Main {
         // OR
         // datatype name_of_variable = value_of_variable; // Initialization (Declaration + Assignment of Value)
 
-        /* DATA TYPES */
+        int points;     // Declaration
+        points = 100;   // Assignment
+        int x = -69;    // Initialization (Declaration + Assignment)
 
-
-        // Integers (Store whole numbers (including negative ones))
-        // Go from -2147483648 to 2147483647
-        int points;
-        points = 100;
-        int x = -69;
-
-
-        // Floating Point Numbers (float & double)
-        float pi = 3.14159265f;
-        double e = 2.718281828;
-
-        // For real numbers, however! Precision can be an issue!
-        // Scientific notation: 314159265 * 10^-8
-
-
-        // Booleans (True or False)
-        boolean likedVideo = true;
-        boolean subscribed = true;
-
-
-        // Chars & Strings
-        char grade = 'B';
-        String name = "Kaupenjoe";
-
-
+        // Every instruction/line ends with a semicolon ;
+        int y = 42;
     }
 }
