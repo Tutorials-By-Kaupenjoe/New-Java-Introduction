c9e5e564c47f5f69303571962a8370dbb8a6cff8
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-11-17 15:55:43 +0100

    data types and variables

--- src/kaupenjoe/Main.java
+++ src/kaupenjoe/Main.java
@@ -2,7 +2,47 @@ package kaupenjoe;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello this is a cool YouTube Series for learning Java!");
+        /* DATA TYPES & VARIABLES */
+
+        // Hello I am a comment
+        /*
+         * And I am a multiline Comment!
+         * I can come in quite handy for explaining stuff!
+         */
+
+        /* Variables */
+
+        // datatype name_of_variable; // Declaration
+        // OR
+        // datatype name_of_variable = value_of_variable; // Initialization (Declaration + Assignment of Value)
+
+        /* DATA TYPES */
+
+
+        // Integers (Store whole numbers (including negative ones))
+        // Go from -2147483648 to 2147483647
+        int points;
+        points = 100;
+        int x = -69;
+
+
+        // Floating Point Numbers (float & double)
+        float pi = 3.14159265f;
+        double e = 2.718281828;
+
+        // For real numbers, however! Precision can be an issue!
+        // Scientific notation: 314159265 * 10^-8
+
+
+        // Booleans (True or False)
+        boolean likedVideo = true;
+        boolean subscribed = true;
+
+
+        // Chars & Strings
+        char grade = 'B';
+        String name = "Kaupenjoe";
+
 
     }
 }
