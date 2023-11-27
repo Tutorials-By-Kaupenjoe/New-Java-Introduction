2810e1164937e14d8df5faaafba1c5182418c64b
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-11-27 15:45:32 +0100

    assignment operators

--- src/kaupenjoe/Main.java
+++ src/kaupenjoe/Main.java
@@ -1,69 +1,38 @@
 package kaupenjoe;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        /* BOOLEANS & LOGIC */
+        /* ASSIGNMENT OPERATORS */
 
-        // Booleans are either true (1) or false (0)
-        boolean goodVideo = true;
-        boolean subscribed = false;
-        System.out.println("This is a good video " + goodVideo);
+        // Assignment =
+        int mana = 100;
+        System.out.println("Mana: " + mana);
 
-        boolean isCreative = false;
-        System.out.println("Player is in Creative Mode: " + isCreative);
+        // ADDITION +=
+        mana += 20; // mana = mana + 20;
+        System.out.println("Mana: " + mana);
 
-        // COMPARISONS (< <= >= > == !=)
+        mana++; // mana += 1; or mana = mana + 1;
+        System.out.println("Mana: " + mana);
 
-        int points = 47;
-        int pointsToPass = 55;
-        boolean hasPassedTest = points >= pointsToPass; // Bigger or Equals
-        System.out.println("Has passed the Test " + hasPassedTest);
+        // SUBTRACTION -=
+        mana -= 60;
+        System.out.println("Mana: " + mana);
 
-        int health = 0;
-        boolean isAlive = health > 0;
-        System.out.println("Is Player Kaupenjoe still alive? " + isAlive);
+        mana--;
+        System.out.println("Mana: " + mana);
 
-        boolean test = 100 == 100; // true
-        test = 100 != 100; // false
+        // MULTIPLY *=
+        mana *= 2;
+        System.out.println("Mana: " + mana);
 
-        // LOGIC (AND OR NOT)
+        // DIVIDE /=
+        mana /= 4;
+        System.out.println("Mana: " + mana);
 
-        // OR
-        // a | b | c
-        // 0 | 0 | 0
-        // 1 | 0 | 1
-        // 0 | 1 | 1
-        // 1 | 1 | 1
-
-        // AND
-        // a | b | c
-        // 0 | 0 | 0
-        // 1 | 0 | 0
-        // 0 | 1 | 0
-        // 1 | 1 | 1
-
-        // NOT
-        // a | !a
-        // 0 |  1
-        // 1 |  0
-
-        boolean passedTest1 = true;
-        boolean passedTest2 = true;
-        boolean hasHonors = passedTest1 && passedTest2; // true if BOTH are true!
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your age?");
-        int age = scanner.nextInt();
-        System.out.println("Put in true or false, are you from Europe?");
-        boolean isEuropean = scanner.nextBoolean();
-
-        int euDrinkingAge = 18;
-        int usDrinkingAge = 21;
-
-        boolean canDrink = isEuropean && age >= euDrinkingAge || !isEuropean && age >= usDrinkingAge;
-        System.out.println("Can you drink? " + canDrink);
+        // MODULO (REMAINDER) %=
+        mana %= 2;
+        System.out.println("Mana: " + mana);
 
     }
 }
