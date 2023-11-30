9d13ebbb1251e537aa9b3d9d800efbb2dfef155c
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-11-30 18:24:04 +0100

    methods

--- src/kaupenjoe/Main.java
+++ src/kaupenjoe/Main.java
@@ -2,26 +2,10 @@ package kaupenjoe;
 
 public class Main {
     public static void main(String[] args) {
-        /* ARRAYS */
+        /* METHODS */
 
-        String question1 = "What does WWW stand for?";
-        String question2 = "What is the World's largest Ocean?";
-        String question3 = "Which Year did East and West Germany Unify?";
-        String question4 = "What is the capital of France?";
+        // Going Beyond the main method!
 
-        String answer1 = "World Wide Web";
-        String answer2 = "Pacific Ocean";
-        String answer3 = "1990";
-        String answer4 = "Paris";
-
-        // Now add 15 more...
-        // Let's loop through them
-
-        for(int i = 0; i < 4; i++) {
-            // System.out.println(questioni); DOES NOT WORK!
-        }
-
-        // Array - Think of this as a list
         String[] questions = new String[4];
         questions[0] = "What does WWW stand for?";
         questions[1] = "What is the World's largest Ocean?";
@@ -34,14 +18,30 @@ public class Main {
         answers[2] = "1990";
         answers[3] = "Paris";
 
-        System.out.println("Q: " + questions[0] + " | A: " + answers[0]);
+        outputQuestionsAndAnswersXTimes(5, questions, answers);
+        System.out.println("----");
+        outputQuestionsAndAnswersXTimes(2, questions, answers);
+        System.out.println("----");
+        outputQuestionsAndAnswersXTimes(3, questions, answers);
+        System.out.println("----");
+        outputQuestionsAndAnswersXTimes(8, questions, answers);
 
+
+    }
+
+    private static void outputQuestionsAndAnswersXTimes(int x, String[] questions, String[] answers) {
+        for (int c = 0; c < x; c++) {
+            outputQuestionsAndAnswers(questions, answers);
+        }
+    }
+
+    public static void outputQuestionsAndAnswers(String[] questions, String[] answers) {
         for(int i = 0; i < questions.length; i++) {
             System.out.println("Q: " + questions[i] + " | A: " + answers[i]);
         }
+    }
 
-        for(String question : questions) {
-            System.out.println(question);
-        }
+    public static int sum(int x, int y) {
+        return x + y;
     }
 }
