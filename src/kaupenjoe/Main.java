e5334de43ad95b3e6954740dd0cdfd1e6eb1258a
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-12-03 15:19:28 +0100

    anonymous classes

--- src/kaupenjoe/Main.java
+++ src/kaupenjoe/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        /* INTERFACES & ABSTRACT CLASSES */
+        /* ANONYMOUS CLASSES */
 
         Animal bengie = new Dog("bengie.png", "Bengie", 9);
         Animal jeremy = new Dog("jeremy.png", "Jeremy", 7);
@@ -14,14 +14,32 @@ public class Main {
 
         Animal george = new Cat("george.jpg", "George", 3);
 
-        Bird jules = new Bird("jules.png", "Jules", 1);
+        Bird jules = new Bird("jules.png", "Jules", 1) {
+            @Override
+            public void move() {
+                super.move();
+            }
+        };
         jules.move();
         jules.fly();
+
+        Animal joel = new Animal("joel.jpg", "Joel", 2) {
+            @Override
+            public void move() {
+                System.out.println("Hamster is running in his wheel!");
+            }
+
+            @Override
+            public void makeSound() {
+                System.out.println("Very tiny hamster noises");
+            }
+        };
 
         List<Animal> animals = new ArrayList<>();
         animals.add(bengie);
         animals.add(whiskers);
         animals.add(jules);
+        animals.add(joel);
 
         List<IFlyable> flyables = new ArrayList<>();
         flyables.add(jules);
