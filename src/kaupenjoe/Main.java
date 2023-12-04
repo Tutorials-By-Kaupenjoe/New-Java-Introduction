e726d1436a91ee3af34ad97287269bf4a4ac6e62
Kaupenjoe <N.Kaupenjohann@gmail.com>
2023-12-04 18:18:11 +0100

    more things to consider

--- src/kaupenjoe/Main.java
+++ src/kaupenjoe/Main.java
@@ -1,9 +1,22 @@
 package kaupenjoe;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        /* JSON */
+        /* LAST TUTORIAL */
 
+        final int x = 30;
+        List<String> names = null;
+
+        var y = 5.5;
+
+        /* MORE THINGS TO LOOK AT */
+
+        // Design Patterns for Java
+        // Singleton Pattern
+
+        // Java Conventions
 
 
     }
